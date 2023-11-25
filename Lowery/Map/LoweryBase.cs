6573e2c08e145555cdd785211e050bb5539efb3d
Kyle Deutsch <kyle.deutsch100@gmail.com>
2023-11-25 15:35:13 -0800

    Base updates

--- Lowery/Map/LoweryBase.cs
+++ Lowery/Map/LoweryBase.cs
@@ -23,5 +23,37 @@ namespace Lowery
             Table table = await QueuedTask.Run(() => { return DisplayTable.GetTable(); });
             return await table.Get<T>(filter);
 		}
-	}
+
+		public async Task<long> Insert<T>(T value) where T : class, new()
+		{
+            if (DisplayTable == null)
+                return -1;
+            Table table = await QueuedTask.Run(() => { return DisplayTable.GetTable(); });
+			return await table.Insert<T>(value);
+        }
+
+		public async Task Update<T>(T value) where T : class, new()
+		{
+            if (DisplayTable == null)
+                return;
+            Table table = await QueuedTask.Run(() => { return DisplayTable.GetTable(); });
+            await table.Update<T>(value);
+        }
+
+		public async Task Delete<T>(T value) where T : class, new()
+		{
+			if (DisplayTable == null)
+				return;
+			Table table = await QueuedTask.Run(() => { return DisplayTable.GetTable(); });
+			await table.Delete<T>(value);
+		}
+
+		public async Task DeleteMany<T>(IEnumerable<T> values) where T : class, new()
+		{
+            if (DisplayTable == null)
+                return;
+            Table table = await QueuedTask.Run(() => { return DisplayTable.GetTable(); });
+			//await table.DeleteMany<T>(values);
+        }
+    }
 }
